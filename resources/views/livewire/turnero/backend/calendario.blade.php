66ca88cb84dde0337ad44aa91a7a5dd95c922a62
o  | |  o <90270276+Agugodzic@users.noreply.github.com>
2023-09-29 15:21:30 -0300

    correccion errores, cambio de estilos

--- resources/views/livewire/turnero/backend/calendario.blade.php
+++ resources/views/livewire/turnero/backend/calendario.blade.php
@@ -59,16 +59,16 @@
         <?php $i = 0; ?>
 
         @foreach($calendario as $dia)
-        <?php $i++; ?>
-        <button class="item-calendario {{$dia['enmes']}} {{$dia['turnos']}}"
-          wire:click="fechaSelect('{{ $dia['fecha']}}')">
-          {{ $dia['dia'] }}</button>
-        <?php
-                          if($i == 7){
-                            echo '<br/>';
-                            $i = 0;
-                          }
-                        ?>
+            <?php $i++; ?>
+            <button class="item-calendario {{$dia['enmes']}} {{$dia['turnos']}}"
+            wire:click="fechaSelect('{{ $dia['fecha']}}')">
+            {{ $dia['dia'] }}</button>
+            <?php
+                if($i == 7){
+                echo '<br/>';
+                $i = 0;
+                }
+            ?>
         @endforeach
       </div>
 
@@ -77,9 +77,14 @@
       <div class="row mt-2 turnos-container">
         <div @if(!$fechaSeleccionada) style="display:none" @endif class="mt-2">
           <h4 class="mt-4 mb-3 turnos-titulo">Turnos para el {{ $fechaSelFormateada }} </h4>
+            <style>
+                .turnos-table::-webkit-scrollbar-track {
+                    background-color: rgba(128, 128, 128, 0);
+                }
 
+            </style>
           <div class="table-responsive turnos-table">
-            <table class="table table-vcenter card-table" style="min-width:max-content;">
+            <table class="table table-vcenter card-table" style="min-width:max-content;margin-bottom:25px !important;">
 
               <thead>
                 <tr>
@@ -98,80 +103,89 @@
               <tbody>
                 @foreach($turnos as $turno)
                 <tr class="table-filas">
-                    @foreach($turno['patologias'] as $index => $item)
+                        <td class="turno-patologia" style="max-width: 120px;padding:8px;!important;">
+                        @if($turno['paciente'] && $turno['patologias'])
+                            @foreach($turno['patologias'] as $index => $item)
 
-                    <td class="turno-patologia" style="max-width: 120px;padding: 8px;">  @if($item->patologia){{ $item->patologia->dolencia}}@endif </td>
+                            @if(isset($item->patologia))<p style="margin-bottom:5px">{{ $item->patologia->dolencia}}</p>@endif
 
-                    @endforeach
+                            @endforeach
+                        @endif
+                    </td>
 
                     <td class="turno-hora">{{ $turno['hora'] }}</td>
 
                     <td class="turno-paciente">
                         @if($turno['paciente'])
-                            <a href="{{route('pacientes.turnero.edit',$turno['pacienteId'])}}">{{ $turno['paciente'] }}</a>
-                            <a href="https://wa.me/{{ $turno['telefono'] }}" target="_blank">
-                            <img class="ms-2" src="{{ asset('img/logo-whatsapp.png')}}" width="25" /></a>
+                            <a style="margin-right:10px;" href="{{route('pacientes.turnero.edit',$turno['pacienteId'])}}">{{ $turno['paciente'] }}</a>
                         @endif
+
+                        @if($turno['paciente'])
+                            <a style="max-width:fit-content;padding:7px 0px;" class="btn btn-ghost-light btn-icon" href="https://wa.me/{{ $turno['telefono'] }}" target="_blank" title="Hablar por Whatsapp">
+                            <img src="{{ asset('img/logo-wsp-b.png')}}" height="19" style="opacity:60%"/></a>
+                        @endif
+
                         @if($turno['comprobante_pago'])
-                        @if ($turno['comprobante_pago'] == "mercadopago")
-                            <span class="badge bg-azure">MP</span>
-                        @else
-                            <a href="{{ url('img/uploads/' . $turno['comprobante_pago']) }}" target="_blank"><img src="{{ asset('img/icon-decl.png')}}" width="25"/></a>
+                            @if ($turno['comprobante_pago'] == "mercadopago")
+                                <span class="badge bg-azure">MP</span>
+                            @else
+                                <a title="Ver comprobante de pago" class="btn btn-ghost-light btn-icon" style="max-width:fit-content;padding:7px 0px;" href="{{ url('img/uploads/' . $turno['comprobante_pago']) }}" target="_blank"><img src="{{ asset('img/comprobante.png')}}" width="13"/></a>
+                            @endif
                         @endif
-                        @endif
+
                         @if($turno['fichaId'])
-                            <a class="btn btn-ghost-light btn-icon" href="{{route('pacientes.edit',$turno['fichaId'])}}"
-                            data-toggle="tooltip" data-placement="right" title="Ficha del Paciente">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-notes" width="24"
-                                height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                                stroke-linecap="round" stroke-linejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                <rect x="5" y="3" width="14" height="18" rx="2"></rect>
-                                <line x1="9" y1="7" x2="15" y2="7"></line>
-                                <line x1="9" y1="11" x2="15" y2="11"></line>
-                                <line x1="9" y1="15" x2="13" y2="15"></line>
-                            </svg>
+                            <a class="ficha-button btn btn-ghost-light btn-icon" href="{{route('pacientes.edit',$turno['fichaId'])}}"
+                                data-toggle="tooltip" data-placement="right" title="Ficha del Paciente">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-notes" width="24"
+                                    height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                                    stroke-linecap="round" stroke-linejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                    <rect x="5" y="3" width="14" height="18" rx="2"></rect>
+                                    <line x1="9" y1="7" x2="15" y2="7"></line>
+                                    <line x1="9" y1="11" x2="15" y2="11"></line>
+                                    <line x1="9" y1="15" x2="13" y2="15"></line>
+                                </svg>
                             </a>
-                            <button class="btn btn-ghost-light btn-icon" wire:click="mailFormulario({{ $turno['fichaId'] }})"
-                            data-toggle="tooltip" data-placement="right" title="Enviar Ficha">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-mail" width="24"
-                                height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                                stroke-linecap="round" stroke-linejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                <rect x="3" y="5" width="18" height="14" rx="2"></rect>
-                                <polyline points="3 7 12 13 21 7"></polyline>
-                            </svg>
+
+                            <button class="email-button btn btn-ghost-light btn-icon" wire:click="mailFormulario({{ $turno['fichaId'] }})" data-toggle="tooltip" data-placement="right" title="Enviar Ficha">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-mail" width="24"
+                                    height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                                    stroke-linecap="round" stroke-linejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                    <rect x="3" y="5" width="18" height="14" rx="2"></rect>
+                                    <polyline points="3 7 12 13 21 7"></polyline>
+                                </svg>
                             </button>
                         @endif
                     </td>
 
                     <td class="turno-atendido" style="text-align:center;">
                         @if($turno['paciente'])
-                        @if ($turno['atendido'])
-                        <span class="btn badge bg-success me-1" wire:click="noAtendido('{{ $turno['id'] }}')"></span>Sí
-                        @else
-                        <span class="btn badge bg-danger me-1" wire:click="atendido('{{ $turno['id'] }}')"></span>No
-                        @endif
+                            @if ($turno['atendido'])
+                                <span class="btn badge bg-success me-1" wire:click="noAtendido('{{ $turno['id'] }}')"></span>Sí
+                            @else
+                                <span class="btn badge bg-danger me-1" wire:click="atendido('{{ $turno['id'] }}')"></span>No
+                            @endif
                         @endif
                     </td>
 
                     <td class="turno-pedi-captura" style="text-align:center;">
                         @if($turno['paciente'])
-                        @if ($turno['pedi_captura'])
-                        <span class="btn badge bg-success me-1" wire:click="noPediCaptura('{{ $turno['id'] }}')"></span>Sí
-                        @else
-                        <span class="btn badge bg-danger me-1" wire:click="pediCaptura('{{ $turno['id'] }}')"></span>No
-                        @endif
+                            @if ($turno['pedi_captura'])
+                                <span class="btn badge bg-success me-1" wire:click="noPediCaptura('{{ $turno['id'] }}')"></span>Sí
+                            @else
+                                <span class="btn badge bg-danger me-1" wire:click="pediCaptura('{{ $turno['id'] }}')"></span>No
+                            @endif
                         @endif
                     </td>
 
                     <td class="turno-mando-captura" style="text-align:center;">
                         @if($turno['paciente'])
-                        @if ($turno['mando_captura'])
-                        <span class="btn badge bg-success me-1" wire:click="noMandoCaptura('{{ $turno['id'] }}')"></span>Sí
-                        @else
-                        <span class="btn badge bg-danger me-1" wire:click="mandoCaptura('{{ $turno['id'] }}')"></span>No
-                        @endif
+                            @if ($turno['mando_captura'])
+                                <span class="btn badge bg-success me-1" wire:click="noMandoCaptura('{{ $turno['id'] }}')"></span>Sí
+                            @else
+                                <span class="btn badge bg-danger me-1" wire:click="mandoCaptura('{{ $turno['id'] }}')"></span>No
+                            @endif
                         @endif
                     </td>
 
@@ -191,50 +205,50 @@
                             </svg>
                             </button>
                         @else
-                            <button class="btn btn-ghost-light btn-icon" wire:click="$emit('triggerCancel',{{ $turno['id'] }})"
-                            data-toggle="tooltip" data-placement="right" title="Cancelar Turno">
+                            <button class="btn btn-ghost-light btn-icon" wire:click="$emit('triggerCancel',{{ $turno['id'] }})" data-toggle="tooltip" data-placement="right" title="Cancelar Turno">
                             <!-- Download SVG icon from http://tabler-icons.io/i/user-off -->
-                            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
-                                stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
-                                stroke-linejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                <path d="M14.274 10.291a4 4 0 1 0 -5.554 -5.58m-.548 3.453a4.01 4.01 0 0 0 2.62 2.65" />
-                                <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 1.147 .167m2.685 2.681a4 4 0 0 1 .168 1.152v2" />
-                                <line x1="3" y1="3" x2="21" y2="21" />
-                            </svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
+                                    stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+                                    stroke-linejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                    <path d="M14.274 10.291a4 4 0 1 0 -5.554 -5.58m-.548 3.453a4.01 4.01 0 0 0 2.62 2.65" />
+                                    <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 1.147 .167m2.685 2.681a4 4 0 0 1 .168 1.152v2" />
+                                    <line x1="3" y1="3" x2="21" y2="21" />
+                                </svg>
                             </button>
                         @endif
 
-                        <a class="btn btn-ghost-light btn-icon" href="{{route('turnos.edit',$turno['id']) }}"
-                        data-toggle="tooltip" data-placement="right" title="Editar Turno">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-edit" width="24"
-                            height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                            stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                            <path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1"></path>
-                            <path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z"></path>
-                            <path d="M16 5l3 3"></path>
-                        </svg>
+                        <a class="btn btn-ghost-light btn-icon" href="{{route('turnos.edit',$turno['id']) }}" data-toggle="tooltip" data-placement="right" title="Editar Turno">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-edit" width="24"
+                                height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                                stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1"></path>
+                                <path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z"></path>
+                                <path d="M16 5l3 3"></path>
+                            </svg>
                         </a>
                     </td>
 
-                    <td class="turno-comentarios" style="display:flex;flex-direction:row;align-items:center;max-width:240px">
-                        <textarea class="form-control" rows="1"
-                        id="com-{{$turno['id']}}">{{$turno['comentarios']}}</textarea>
+                    <td class="turno-comentarios">
+                        <div style="display:flex;flex-direction:row;max-width:240px;">
+                            <textarea style="height:100%;" class="form-control"
+                            id="com-{{$turno['id']}}">{{$turno['comentarios']}}</textarea>
 
-                        <button class="btn btn-ghost-light btn-icon"
-                        wire:click="$emit('guardarComentario',{{ $turno['id'] }})" data-toggle="tooltip"
-                        data-placement="right" title="Guardar Comentario">
-                        <!-- Download SVG icon from http://tabler-icons.io/i/user-off -->
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-device-floppy"
-                            width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                            stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                            <path d="M6 4h10l4 4v10a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2"></path>
-                            <circle cx="12" cy="14" r="2"></circle>
-                            <polyline points="14 4 14 8 8 8 8 4"></polyline>
-                        </svg>
-                        </button>
+                            <button class="btn btn-ghost-light btn-icon"
+                            wire:click="$emit('guardarComentario',{{ $turno['id'] }})" data-toggle="tooltip"
+                            data-placement="right" title="Guardar Comentario">
+                            <!-- Download SVG icon from http://tabler-icons.io/i/user-off -->
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-device-floppy"
+                                width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                                stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <path d="M6 4h10l4 4v10a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2"></path>
+                                <circle cx="12" cy="14" r="2"></circle>
+                                <polyline points="14 4 14 8 8 8 8 4"></polyline>
+                            </svg>
+                            </button>
+                        </div>
                     </td>
 
 
