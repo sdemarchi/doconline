68c4035aab86cc110d7965f4cddf72a08c238918
o  | |  o <90270276+Agugodzic@users.noreply.github.com>
2023-10-04 11:08:17 -0300

    Eliminar puntitos borde calendario

--- resources/views/backend/turnero/calendario.blade.php
+++ resources/views/backend/turnero/calendario.blade.php
@@ -6,9 +6,9 @@
     </x-slot>
 
     <div class="py-12" style="width:auto !important; max-width:none !important;">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 border-b sm:rounded-lg border-gray-200">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8"  style="background-color: rgba(0, 0, 0, 0) !important;">
+            <div class="overflow-hidden shadow-sm sm:rounded-lg" style="background-color: rgba(0, 0, 0, 0) !important;">
+                <div class="p-6 border-b sm:rounded-lg border-gray-200"  style="background-color: rgba(0, 0, 0, 0) !important;">
                    <livewire:turnero.backend.calendario/>
                 </div>
             </div>
