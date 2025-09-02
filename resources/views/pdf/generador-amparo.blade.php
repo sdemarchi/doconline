e873ce51d8c918f47a18fa230657dfbfb98557e8
o  | |  o <90270276+Agugodzic@users.noreply.github.com>
2025-09-02 20:17:45 -0300

    arreglo generador amparo

--- resources/views/pdf/generador-amparo.blade.php
+++ resources/views/pdf/generador-amparo.blade.php
@@ -1,20 +1,20 @@
 <style>
     #contenedor-documento{
         font-size: 14px;
-        padding-left:42px;
-        padding-right:42px;
+        padding-left:35px;
+        padding-right:35px;
         font-family: "DejaVu Serif", serif;
     }
 
     h4{
         font-size: 15px;
         margin-bottom: 8px;
-        margin-top:30px;
+        margin-top:25px;
     }
 
     #vocativo{
         font-size: 14px;
-        margin-bottom:30px;
+        margin-bottom:20px;
     }
 
     .inciso p{
@@ -29,6 +29,7 @@
         margin-bottom:40px;
     }
 
+
     @page {
         margin-top: 60px;
     }
@@ -37,16 +38,16 @@
 
 
 <div id="contenedor-documento">
-   <h4 id="titulo">INTERPONE AMPARO POR MORA</h3>
-    <p id="vocativo"><b>SEÑOR JUEZ FEDERAL:</b></p>
+   <h4 id="titulo"><u>INTERPONE AMPARO POR MORA</u></h3>
+    <p id="vocativo"><b>SR JUEZ FEDERAL:</b></p>
     <div class="inciso">
         <p>
-            <b>{{ucwords(strtolower($paciente->nom_ape))}}</b> DNI <b>{{$paciente->dni}}</b> con el patrocinio letrado del <b>Dr. Guillermo Francisco ROBLES</b>, abogado, T° 509 F° 197, CUIT N° 20374758994, con domicilio real en la calle con domicilio real en <b>{{$paciente->domicilio}}, {{ $paciente->localidad}}, {{$paciente->provincia->Provincia}}</b> y procesal constituido en <b>San Martín 77</b> de esta ciudad. Constituyendo domicilio electrónico en el CUIT: 20374758994 (Datos de Contacto: Estudio Jurídico ROBLES - Tel: 3517685657 - Mail: guifranrob@gmail.com), se presenta y respetuosamente dice:
+            <b>{{ucwords(strtolower($paciente->nom_ape))}}</b> DNI <b>{{$paciente->dni}}</b> con el patrocinio letrado del <b>Dr. Guillermo Francisco ROBLES</b>, abogado, T° 509 F° 197, CUIT N° 20374758994, con domicilio real en la calle <b>{{$paciente->domicilio}}, {{ $paciente->localidad}}, {{$paciente->provincia->Provincia}}</b> y procesal constituido en <b>San Martín 77</b> de esta ciudad. Constituyendo domicilio electrónico en el CUIT: 20374758994 (Datos de Contacto: Estudio Jurídico ROBLES - Tel: 3517685657 - Mail: guifranrob@gmail.com), se presenta y respetuosamente dice:
         </p>
     </div>
 
 
-    <h4>I. OBJETO:</h4>
+    <h4 class="titulo-inciso">I. <u>OBJETO:</u></h4>
     <div class="inciso">
         <p>
             Que vengo por la presente a <b>interponer formal Acción de Amparo por Mora</b> conforme el artículo 28 de la Ley 19.549 contra el Registro del Programa de Cannabis (REPROCANN) <b>dependiente del Ministerio de Salud de la Nación</b>, con domicilio en Av. 9 de Julio 1925, Ciudad Autónoma de Buenos Aires, CUIT: 30-54666342-2, en virtud del <b>SILENCIO DE LA ADMINISTRACIÓN</b> ante el pedido de inscripción del amparista ante el registro público que autoriza al autocultivo o cultivo delegado de plantas de cannabis para aquellos pacientes que tengan prescripción médica.
@@ -56,13 +57,16 @@
         </p>
     </div>
 
-    <h4>II. HECHOS:</h4>
+    <h4 class="titulo-inciso">II. <u>HECHOS:</u></h4>
     <div class="inciso">
         <p>
             Con el aval del profesional de salud <b>Dr. Joaquín JOZAMI</b>, el amparista completó la 'Solicitud de Inscripción al Registro Nacional de Pacientes en Tratamiento con Cannabis (REPROCANN)' y firmó el 'Consentimiento Informado Bilateral'.
         </p>
         <p>
             El trámite cuyas constancias para identificación e individualización obran en la documental que se adjunta al presente, <b>al día de hoy se encuentra en estado “PENDIENTE DE EVALUACIÓN”</b> pese a que se encuentran holgadamente cumplidos los plazos previstos por la normativa aplicable.
+        </p>
+        <p>
+            Esta falta de contestación evidencia una conducta pasiva y contraria al deber de la Administración de brindar respuesta a las peticiones formuladas por los particulares, en los plazos y formas que establece la normativa vigente.
         </p>
         <p>
             Ante la falta de respuesta por parte de la Administración, y habiendo transcurrido en exceso el plazo legal establecido por el <b>artículo 10 de la Ley N.º 19.549 de Procedimiento Administrativo</b>, sin que se haya emitido resolución alguna respecto a la solicitud de inscripción en el Registro Nacional,<b> el peticionante se ve en la obligación de recurrir a la presente vía judicial</b> a fin de obtener una respuesta efectiva por parte del Estado.
@@ -79,8 +83,7 @@
     </div>
 
 
-    <h4>III. MARCO NORMATIVO</h4>
-
+    <h4 class="titulo-inciso">III. <u>MARCO NORMATIVO:</u></h4>
     <div class="inciso">
         <p>
             La Ley 27.350, sancionada en marzo de 2017 por el Congreso Nacional, establece medidas para garantizar el derecho a la salud mediante la promoción, prevención, y acceso gratuito a tratamientos con cannabis. Este marco legal se basa en los avances científicos sobre las propiedades medicinales del cannabis y está bajo la supervisión del Ministerio de Salud de la Nación.
@@ -120,7 +123,7 @@
             Asimismo, corresponde remarcar que el Ministerio de Salud de la Nación no ha dado tratamiento a la solicitud en sede administrativa, omitiendo incluso la posibilidad de requerir subsanaciones o complementaciones por parte del profesional tratante, lo cual hubiera permitido regularizar de manera simple y directa cualquier supuesto defecto formal.
         </p>
 
-        <h4>IV. RESOLUCIÓN DE EXPEDIENTES ADMINISTRATIVOS ANÁLOGOS</h4>
+        <h4 class="titulo-inciso">IV. <u>RESOLUCIÓN DE EXPEDIENTES ADMINISTRATIVOS ANÁLOGOS:</u></h4>
         <p>
             En primer lugar, es ampliamente conocido que cientos de miles de usuarios están registrados en el REPROCANN. Es menester destacar las decisiones administrativas tomadas en casos similares, donde otros pacientes con diagnósticos comparables han sido inscriptos en el registro cumpliendo con los requisitos legales vigentes, tal como lo ha hecho el actor. Adoptar una postura divergente respecto a los numerosos casos  análogos  podría  constituir  una  violación  a  principios constitucionales fundamentales como <b>la igualdad ante la ley, la seguridad jurídica y la prohibición de discriminación arbitraria.</b>
         </p>
@@ -135,7 +138,7 @@
         </p>
     </div>
 
-    <h4>V. PROCEDENCIA</h4>
+    <h4 class="titulo-inciso">V. <u>PROCEDENCIA:</u></h4>
     <div class="inciso">
         <p>
             <b>Ley 19.549 de procedimiento administrativo en su art. 28</b> que prevé el presente remedio para la falta de respuesta de la administración. Se han cumplido todos los requisitos exigidos al momento de la inscripción y dentro de los plazos legales.
@@ -147,16 +150,16 @@
             Queda configurado el silencio administrativo en los términos del artículo 10 de la Ley 19.549, en su redacción vigente, sin que resulte exigible la presentación previa de un pronto despacho administrativo, conforme la normativa reformada. En consecuencia, se encuentra habilitada la vía judicial pertinente, por lo que <b>solicitamos se libre pronto despacho judicial a fin de que la Administración se expida expresamente en el marco del procedimiento iniciado.</b>
         </p>
 
-        <h4>VI. RESERVA CASO FEDERAL</h4>
+        <h4 class="titulo-inciso">VI. <u>RESERVA CASO FEDERAL:</u></h4>
         <p>
         Ante el hipotético y poco probable caso que V.S. no dicte sentencia en favor de nuestra pretensión,, se hace desde ya expresa reserva del caso Federal y del derecho a recurrir por ante la Corte Suprema de Justicia de la Nación por la vía del art. 14 de la ley 48 y tribunales internacionales de Derechos Humanos. Ello por cuanto la negativa a la apertura de esta instancia consagraría un daño irreparable, violatorio de derechos y garantías constitucionales individualizados en esta presentación.
         </p>
     </div>
 
-    <h4>VII. PRUEBA</h4>
+    <h4 class="titulo-inciso">VII. <u>PRUEBA:</u></h4>
     <div class="inciso">
-        <h4 style="margin-top:5px">A) I. DOCUMENTAL:</h4>
-        <div>
+        <div style="margin-left: 25px">
+        <h4 style="margin-top:5px">I. DOCUMENTAL:</h4>
             <ol>
                 <li>Documento Nacional de Identidad.</li>
                 <li>Captura de pantalla de la página web oficial de donde surge que el trámite iniciado se encuentra “PENDIENTE DE EVALUACIÓN”.</li>
@@ -164,16 +167,12 @@
                 <li>Captura de pantalla de la inscripción del Dr. JOZAMI en el REFEPS.</li>
             </ol>
 
-            <div style="margin-left: 25px">
-                <h4>II. DOCUMENTAL EN PODER DE LA CONTRAPARTE.</h4>
-                <p>
-                    No obstante, cree mi parte que el presente caso es de puro derecho, para el supuesto que V.S. lo estime imperioso, solicito se intime a la demandada, en los términos del art. 388 del C.P.C.C.N., para que acompañe el expediente respecto a la solicitud de inscripción en el Registro Nacional de Pacientes en Tratamiento con Cannabis (REPROCANN).
-                </p>
-            </div>
-        </div>
+            <h4>II. DOCUMENTAL EN PODER DE LA CONTRAPARTE.</h4>
+            <p>
+                No obstante, cree mi parte que el presente caso es de puro derecho, para el supuesto que V.S. lo estime imperioso, solicito se intime a la demandada, en los términos del art. 388 del C.P.C.C.N., para que acompañe el expediente respecto a la solicitud de inscripción en el Registro Nacional de Pacientes en Tratamiento con Cannabis (REPROCANN).
+            </p>
 
-        <h4>B) INFORMATIVA (en subsidio):</h4>
-        <div>
+            <h4>III. INFORMATIVA (en subsidio)</h4>
             <p>
                 Solicitamos que se libre oficio a la autoridad requerida a fines de que:
                 <ul>
@@ -184,20 +183,26 @@
         </div>
     </div>
 
-    <h4>VIII. SOLICITUD DE PLAZO UNIFORME PARA DEOX</h4>
+    <h4 class="titulo-inciso">VIII. <u>SOLICITUD DE PLAZO UNIFORME PARA DEOX</u></h4>
     <div class="inciso">
         <p>
-            Solicito a V.S. que <b>el plazo prudencial para el DEOX no exceda los cinco días hábiles, independientemente de la jurisdicción territorial</b>, atento que la digitalización integral del sistema judicial federal ha eliminado toda justificación objetiva para diferenciaciones territoriales y ampliar plazos en razón de la distancia viola el principio constitucional de igualdad entre todos los habitantes de la Nación (art. 16 CN). La totalidad de las actuaciones se tramitan mediante plataformas electrónicas donde la notificación es instantánea e idéntica en todo el territorio nacional, siendo anacrónico mantener criterios basados en distancias físicas inexistentes en el mundo digital.
+            <b>Solicito a V.S. que el plazo prudencial para el DEOX no exceda los cinco días hábiles, independientemente de la jurisdicción territorial,</b> conforme a los principios de celeridad y eficacia consagrados en el nuevo artículo 28 de la Ley 19.549, sustituido por el artículo 47 de la Ley 27.742 (Ley de Bases), que establece expresamente que <b>"el juez requerirá a la autoridad administrativa interviniente que en el plazo de cinco (5) días hábiles judiciales informe las causas de la demora”.</b>
         </p>
         <p>
-            La naturaleza sumaria del amparo y el plazo de apelación de apenas 48 horas evidencian que cualquier DEOX superior a cinco días resulta desproporcionado e incompatible con la urgencia constitucional de este instituto destinado a proteger derechos fundamentales. El plazo prudencial del artículo 8° de la Ley 16.986 debe interpretarse conforme esta realidad tecnológica contemporánea que garantiza acceso igualitario y efectivo a la tutela judicial sin discriminaciones territoriales carentes de sustento fáctico.
+            Esta normativa introduce un criterio uniforme de cinco días hábiles que debe regir en todo el territorio nacional, eliminando las diferenciaciones territoriales anacrónicas que contradicen el principio constitucional de igualdad entre todos los habitantes de la Nación (art. 16 CN). La digitalización integral del sistema judicial federal ha tornado objetivamente injustificable cualquier distinción basada en distancias físicas inexistentes en el mundo digital, donde la totalidad de las actuaciones se tramitan mediante plataformas electrónicas con notificación instantánea e idéntica en todo el territorio nacional.
         </p>
         <p>
-            En consecuencia, corresponde que V.S. unifique el plazo en cinco días hábiles, eliminando diferenciaciones que violan la igualdad ante la ley y contradicen los principios de celeridad propios del amparo por mora administrativa.
+            El nuevo régimen legal del amparo por mora administrativa refleja una decisión legislativa clara de privilegiar la celeridad y la uniformidad procedimental. La naturaleza sumaria del amparo y el plazo de apelación de apenas 48 horas evidencian que cualquier DEOX superior a cinco días resulta desproporcionado e incompatible con la urgencia constitucional de este instituto destinado a proteger derechos fundamentales.
+        </p>
+        <p>
+            La reforma introducida por la Ley de Bases establece un estándar objetivo de cinco días hábiles que debe interpretarse como el parámetro máximo de razonabilidad para todo el territorio nacional, garantizando acceso igualitario y efectivo a la tutela judicial sin discriminaciones territoriales carentes de sustento fáctico en la era digital.
+        </p>
+        <p>
+            <b>En consecuencia, corresponde que V.S. unifique el plazo en cinco días hábiles conforme al nuevo estándar legal, eliminando diferenciaciones que violan la igualdad ante la ley y contradicen los principios de celeridad y eficacia propios del amparo por mora administrativa, tal como lo establece expresamente el artículo 47 de la Ley 27.742.</b>
         </p>
     </div>
 
-    <h4>IX. PETITORIO</h4>
+    <h4 class="titulo-inciso">IX. <u>PETITORIO:<u></h4>
     <div class="inciso">
         <ol>
             Conforme lo expuesto, solicito a V.S. que:
